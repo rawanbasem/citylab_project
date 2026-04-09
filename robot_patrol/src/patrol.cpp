15095d15ab2fea5b64197242d81aea0ac83c77c7
Rawan Basem <rawanbasem202@gmail.com>
2026-04-09 21:19:19 +0000

    Created publisher and timer_callback

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include <cmath>
 #include <chrono>
 
@@ -11,12 +12,19 @@ public:
         
         laser_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
             "/fastbot_1/scan", 10, std::bind(&Patrol::laserscan_callback, this, std::placeholders::_1));
+
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/fastbot_1/cmd_vel", 10);
+
+        // 10 Hz control loop
+        timer_ = this->create_wall_timer(100ms, std::bind(&Patrol::timer_callback, this));
         
         RCLCPP_INFO(this->get_logger(), "Robot Patrol Node started...");
     }
 
 private:
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
     double direction_ = 0.0;
 
@@ -25,7 +33,7 @@ private:
         int middle_index = msg->ranges.size() / 2;
         float forward_dist = msg->ranges[middle_index];
 
-        if (std::isfinite(forward_dist) && forward_dist < 0.45) {
+        if (std::isfinite(forward_dist) && forward_dist < 0.55) {
             
             int first_index = msg->ranges.size() / 4;
             int last_index = 3 * (msg->ranges.size()) / 4;
@@ -45,6 +53,13 @@ private:
             // Path is clear 
             this->direction_ = 0.0;
         }
+    }
+
+    void timer_callback() {
+        auto msg = geometry_msgs::msg::Twist();
+        msg.linear.x = 0.1;               
+        msg.angular.z = direction_ / 2.0; 
+        publisher_->publish(msg);
     }
 };
 
