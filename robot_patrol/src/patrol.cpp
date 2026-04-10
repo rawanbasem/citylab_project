dfe652443edbc7fbee5c36d21bef16556771c066
Rawan Basem <rawanbasem202@gmail.com>
2026-04-10 11:15:31 +0000

    changed logic after testing on real robot, and changed topics

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -10,10 +10,12 @@ class Patrol : public rclcpp::Node {
 public:
     Patrol() : Node("robot_patrol_node") {
         
+        // Subscribing to the simulation's laser scan topic
         laser_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-            "/fastbot_1/scan", 10, std::bind(&Patrol::laserscan_callback, this, std::placeholders::_1));
+            "/scan", 10, std::bind(&Patrol::laserscan_callback, this, std::placeholders::_1));
 
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/fastbot_1/cmd_vel", 10);
+        // Publishing to the simulation's velocity topic
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 
         // 10 Hz control loop
         timer_ = this->create_wall_timer(100ms, std::bind(&Patrol::timer_callback, this));
@@ -30,31 +32,58 @@ private:
 
     void laserscan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
         
-        int middle_index = msg->ranges.size() / 2;
-        float forward_dist = msg->ranges[middle_index];
+        int size = msg->ranges.size();
+        int quarter_index = size / 4;
+        bool obstacle_in_front = false;
+        int cone_size = 10; // Check 10 rays on each side of the front
 
-        if (std::isfinite(forward_dist) && forward_dist < 0.55) { //0.55 is tolerance
-            
-            int first_index = msg->ranges.size() / 4;
-            int last_index = 3 * (msg->ranges.size()) / 4;
+        // Check the front-left
+        for (int i = 0; i <= cone_size; i++) {
+            if (std::isfinite(msg->ranges[i]) && msg->ranges[i] < 0.55) {
+                obstacle_in_front = true;
+            }
+        }
+        //Check the front-right
+        for (int i = size - cone_size; i < size; i++) {
+            if (std::isfinite(msg->ranges[i]) && msg->ranges[i] < 0.55) {
+                obstacle_in_front = true;
+            }
+        }
+
+        if (obstacle_in_front) {
             float max_dist = -1.0;
-            int max_idx = middle_index; 
+            int max_idx = 0; 
 
-            // Find safest direction 
-            for (int i = first_index; i <= last_index; i++) {
+            // Search left side (0 to pi/2)
+            for (int i = 0; i <= quarter_index; i++) {
                 if (std::isfinite(msg->ranges[i]) && msg->ranges[i] > max_dist) {
                     max_dist = msg->ranges[i];
                     max_idx = i; 
                 }    
             }
-            this->direction_ = msg->angle_min + (max_idx * msg->angle_increment);
+            
+            // Search right side (3pi/2 to 2pi)
+            for (int i = size - quarter_index; i < size; i++) {
+                if (std::isfinite(msg->ranges[i]) && msg->ranges[i] > max_dist) {
+                    max_dist = msg->ranges[i];
+                    max_idx = i; 
+                }    
+            }
+            
+            double raw_angle = msg->angle_min + (max_idx * msg->angle_increment);
+            
+            // normalize angles
+            if (raw_angle > M_PI) {
+                raw_angle -= 2 * M_PI;
+            }
+            
+            this->direction_ = raw_angle;
         } 
         else {
-            // Path is clear 
+            // Path is clear
             this->direction_ = 0.0;
         }
     }
-
     void timer_callback() {
         auto msg = geometry_msgs::msg::Twist();
         msg.linear.x = 0.1;               
